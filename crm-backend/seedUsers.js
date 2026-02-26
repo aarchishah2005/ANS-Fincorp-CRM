261760e86ac8692475327f313472e9e6af89c42c
Aarchi Shah <aarchi.shah2005@gmail.com>
2026-02-26 18:59:48 +0530

    Update seedUsers.js

--- crm-backend/seedUsers.js
+++ crm-backend/seedUsers.js
@@ -1,44 +1,47 @@
-require("dotenv").config();
-const connectDB = require("./config/db");
-const User = require("./models/User");
+// Dont use this file for seedUser
+// use seed file in utils folder  
 
-const seedUsers = async () => {
-  try {
-    await connectDB();
+// require("dotenv").config();
+// const connectDB = require("./config/db");
+// const User = require("./models/User");
 
-    await User.deleteMany({});
+// const seedUsers = async () => {
+//   try {
+//     await connectDB();
 
-    const admin1 = new User({
-      name: "Aarchi Shah",
-      email: "aarchi.shah2005@gmail.com",
-      password: "Aarchi@123",
-      role: "admin",
-    });
+//     await User.deleteMany({});
 
-    const sales = new User({
-      name: "Swayam Shah",
-      email: "swayam.shah2010@gmail.com",
-      password: "Swayam@123",
-      role: "sales",
-    });
+//     const admin1 = new User({
+//       name: "Aarchi Shah",
+//       email: "aarchi.shah2005@gmail.com",
+//       password: "Aarchi@123",
+//       role: "admin",
+//     });
 
-    const sales2 = new User({
-      name: "Jitubhai",
-      email: "jitubhai@gmail.com",
-      password: "Jitubhai@123",
-      role: "sales",
-    });
+//     const sales = new User({
+//       name: "Swayam Shah",
+//       email: "swayam.shah2010@gmail.com",
+//       password: "Swayam@123",
+//       role: "sales",
+//     });
 
-    await admin1.save();   // triggers hashing
-    await sales.save();    // triggers hashing
-    await sales2.save();   // triggers hashing
+//     const sales2 = new User({
+//       name: "Jitubhai",
+//       email: "jitubhai@gmail.com",
+//       password: "Jitubhai@123",
+//       role: "sales",
+//     });
 
-    console.log("✅ 3 Users seeded successfully with hashed passwords!");
-    process.exit();
-  } catch (error) {
-    console.error(error);
-    process.exit(1);
-  }
-};
+//     await admin1.save();   // triggers hashing
+//     await sales.save();    // triggers hashing
+//     await sales2.save();   // triggers hashing
 
-seedUsers();
+//     console.log("✅ 3 Users seeded successfully with hashed passwords!");
+//     process.exit();
+//   } catch (error) {
+//     console.error(error);
+//     process.exit(1);
+//   }
+// };
+
+// seedUsers();
